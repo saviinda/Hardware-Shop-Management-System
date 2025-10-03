63f312a8c0844abb6185eb3193eb3956904bcc71
saviinda <savi077nda@gmail.com>
2025-10-03 10:49:03 +0530

    Project started

--- Hardware Shop Management System/Form1.cs
+++ Hardware Shop Management System/Form1.cs
@@ -6,5 +6,10 @@ namespace Hardware_Shop_Management_System
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
